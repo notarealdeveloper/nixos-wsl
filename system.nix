757f0ec15a3a880e5ec1ce8ae189a316bc0c5172
System administrator <root@nixos.localdomain>
2025-07-21 22:43:32 +0000

    Hello world

---
+++ system.nix
@@ -0,0 +1,18 @@
+(pkgs: with pkgs; [
+    vim
+    git
+    gh
+    cowsay
+    xcowsay
+    xorg.xeyes
+    figlet
+    toilet
+    lolcat
+    cmatrix
+    asciiquarium
+    gh
+    xdg-utils
+    google-chrome
+    xclip
+    sl
+])
